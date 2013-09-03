e876f36f4d671794a1929ec85c783885307993b7
jeppeter <jeppeter@gmail.com>
2013-09-03 13:30:16 +0800

    to add strtohex into repo

---
+++ dnf/wp/common/strtohex.h
@@ -0,0 +1,21 @@
+
+
+#ifndef __STR_TO_HEX_H__
+#define __STR_TO_HEX_H__
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+
+unsigned int StrToHex(const char* pStart);
+unsigned long long StrToHexLL(const char* pStart);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*__STR_TO_HEX_H__*/
+
+
--- dnf/wp/wp/main.cpp
+++ dnf/wp/wp/main.cpp
@@ -19,11 +19,12 @@ void Usage(int ec,const char* fmt,...)
     }
 
     fprintf(fp,"wp [OPTIONS]\n");
-    fprintf(fp,"\t-p processid       | to set for process id\n");
-    fprintf(fp,"\t-r readaddr        | to read address\n");
-    fprintf(fp,"\t-w writeaddr       | set write address\n");
-    fprintf(fp,"\t-s size            | read or write size\n");
-    fprintf(fp,"\t-c content         | write content it is in the value\n");
+	fprintf(fp,"\t-h|--help                    | to display this help information\n");
+    fprintf(fp,"\t-p|--process processid       | to set for process id\n");
+    fprintf(fp,"\t-r|--read readaddr           | to read address\n");
+    fprintf(fp,"\t-w|--write writeaddr         | set write address\n");
+    fprintf(fp,"\t-s|--size size               | read or write size\n");
+    fprintf(fp,"\t-c|--content content         | write content it is in the value\n");
     //fprintf(fp,"\t-f filecontent     | write content from file\n");
     //fprintf(fp,"\t-l loaddll         | to load dll\n");
     //fprintf(fp,"\t-f funcname        | to call function name\n");
@@ -40,62 +41,152 @@ static unsigned char* pWriteBuffer=NULL;
 #ifdef _UNICODE
 char* ChangeParam(int argc,wchar* argvw[])
 {
-	int ret;
-	char** pRetArgv=NULL;
-	int retargc=argc,i;
-	int *pRetArgvLen=NULL;
+    int ret;
+    char** pRetArgv=NULL;
+    int retargc=argc,i;
+    int *pRetArgvLen=NULL;
 
-	pRetArgvLen = calloc(sizeof(*pRetArgvLen),argc);
-	if (pRetArgvLen == NULL)
-	{
-		ret = LAST_ERROR_CODE();
-		goto fail;
-	}
+    pRetArgvLen = calloc(sizeof(*pRetArgvLen),argc);
+    if(pRetArgvLen == NULL)
+    {
+        ret = LAST_ERROR_CODE();
+        goto fail;
+    }
 
-	pRetArgv = calloc(sizeof(*pRetArgv),argc);
-	if (pRetArgv == NULL)
-	{
-		ret = LAST_ERROR_CODE();
-		goto fail;
-	}
+    pRetArgv = calloc(sizeof(*pRetArgv),argc);
+    if(pRetArgv == NULL)
+    {
+        ret = LAST_ERROR_CODE();
+        goto fail;
+    }
 
-	if (pRetArgvLen)
-	{
-		free(pRetArgvLen);
-	}
-	pRetArgvLen = NULL;
-	return pRetArgv;
+    for(i=0; i<argc; i++)
+    {
+        ret = UnicodeToAnsi(argvw[i],&(pRetArgv[i]),&(pRetArgvLen[i]));
+        if(ret < 0)
+        {
+            ret = LAST_ERROR_CODE();
+            goto fail;
+        }
+    }
+
+    if(pRetArgvLen)
+    {
+        free(pRetArgvLen);
+    }
+    pRetArgvLen = NULL;
+    return pRetArgv;
 fail:
-	if (pRetArgv)
-	{
-		for(i=0;i<retargc;i++)
-		{
-			assert(pRetArgvLen);
-			if (pRetArgv[i])
-			{
-				free(pRetArgv[i]);
-			}
-			pRetArgv[i] = NULL;			
-		}
+    if(pRetArgv)
+    {
+        for(i=0; i<retargc; i++)
+        {
+            assert(pRetArgvLen);
+            if(pRetArgv[i])
+            {
+                free(pRetArgv[i]);
+            }
+            pRetArgv[i] = NULL;
+        }
 
-		free(pRetArgv);		
-	}
+        free(pRetArgv);
+    }
 
-	if (pRetArgvLen)
-	{
-		free(pRetArgvLen);
-	}
-	pRetArgvLen = NULL;
-	SetLastError(ret);
-	return NULL;	
+    if(pRetArgvLen)
+    {
+        free(pRetArgvLen);
+    }
+    pRetArgvLen = NULL;
+    SetLastError(ret);
+    return NULL;
 }
 #endif
 
 int ParseParam(int argc,char* argv[])
 {
+	int i;
+	for (i=1;i<argc;i++)
+	{
+		if (strcmp(argv[i],"-h")==0 ||
+			strcmp(argv[i],"--help")==0)
+		{
+			Usage(0,NULL);
+		}
+		else if (strcmp(argv[i],"-p")==0 ||
+			strcmp(argv[i],"--process")==0)
+		{
+			if (argc <= (i+1))
+			{
+				Usage(3,"argv[%d] %s need an arg",i,argv[i]);
+			}
+			st_ProcessId = atoi(argv[i+1]);
+			i += 1;
+		}
+		else if (strcmp(argv[i],"-r")==0 ||
+			strcmp(argv[i],"--read")==0)
+		{
+			if (argc <= (i+1))
+			{
+				Usage(3,"argv[%d] %s need an arg",i,argv[i]);
+			}
+			st_ReadAddr = strtoul();
+			
+			
+		}
+		else if (strcmp(argv[i],"")==0 ||
+			strcmp(argv[i],"")==0)
+		{
+		}
+		else if (strcmp(argv[i],"")==0 ||
+			strcmp(argv[i],"")==0)
+		{
+		}
+		else if (strcmp(argv[i],"")==0 ||
+			strcmp(argv[i],"")==0)
+		{
+		}
+		else
+		{
+			Usage(3,"unknown params %s",argv[i]);
+		}
+	}
 }
 
-int main(int argc,TCHAR argv[])
+int main(int argc,TCHAR* argv[])
 {
-    return 0;
+    int i;
+	int ret;
+#ifdef _UNICODE
+    char** pRetArgv=NULL;
+
+	pRetArgv = ChangeParam(argc,argv);
+	if (pRetArgv == NULL)
+	{
+		ret = -1 * LAST_ERROR_CODE();
+		goto out;
+	}
+	ParseParam(argc,pRetArgv);
+#else
+	ParseParam(argc,argv);
+#endif
+
+	
+
+out:
+#ifdef _UNICODE
+    if(pRetArgv)
+    {
+        for(i=0; i<argc; i++)
+        {
+            if(pRetArgv[i])
+            {
+                free(pRetArgv[i]);
+            }
+            pRetArgv[i] = NULL;
+        }
+        free(pRetArgv);
+    }
+    pRetArgv = NULL;
+#endif
+    return ret;
 }
