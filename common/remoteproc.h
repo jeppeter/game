32a9c19764fe1fedfbf66a93475197f4efbd1ad5
jeppeter <jeppeter@gmail.com>
2013-09-05 22:41:38 +0800

    to add the EnableDebugLevel in the file as it will call open process ok

--- common/remoteproc.h
+++ common/remoteproc.h
@@ -12,7 +12,15 @@ int ProcKill(unsigned int processid,int force);
 int ProcEnum(const char* exename,unsigned int**ppPids,int *pSize);
 int ProcMemorySize(unsigned int processid,unsigned int *pMemSize);
 
-int RaiseTokenPriviledge();
+/******************************************
+* input
+*          enable == 0 disabled ,enable == 1 for enabled
+* return value:
+*         1 origin is enabled
+*         0 origin is disabled
+*         negative error code
+******************************************/
+int EnableDebugLevel(int enble);
 
 #ifdef __cplusplus
 };
