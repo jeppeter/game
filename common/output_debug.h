46096dc0d9749d52125dc9fc2ec063b1602ed0b3
jeppeter <jeppeter@gmail.com>
2013-09-06 14:04:01 +0800

    make multiple debug outstring

--- common/output_debug.h
+++ common/output_debug.h
@@ -9,9 +9,21 @@
 extern "C" {
 #endif
 
+
+#ifdef WIN7_VER
 #define  DEBUG_INFO(fmt,...) DebugOutString(__FILE__,__LINE__,fmt,__VA_ARGS__)
 #define  ERROR_INFO(fmt,...) DebugOutString(__FILE__,__LINE__,fmt,__VA_ARGS__)
+#else
+#ifdef WINXP_VER
+#define  DEBUG_INFO(fmt,...) do{fprintf(stderr,fmt,__VA_ARGS__);DebugOutString(__FILE__,__LINE__,fmt,__VA_ARGS__);}while(0)
+#define  ERROR_INFO(fmt,...) do{fprintf(stderr,fmt,__VA_ARGS__);DebugOutString(__FILE__,__LINE__,fmt,__VA_ARGS__);}while(0)
+#else
+#define  DEBUG_INFO(fmt,...) do{;}while(0)
+#define  ERROR_INFO(fmt,...) do{;}while(0)
+#endif
+#endif
 #define  DEBUG_BUFFER(ptr,blen) DebugBuffer(__FILE__,__LINE__,(unsigned char*)ptr,blen)
+
 
 extern "C" void DebugOutString(const char* file,int lineno,const char* fmt,...);
 extern "C" void DebugBuffer(const char* file,int lineno,unsigned char* pBuffer,int buflen);
