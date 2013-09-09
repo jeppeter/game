d7ba465fb3bed887a4f26223cca15464709576a4
jeppeter <jeppeter@gmail.com>
2013-09-09 22:46:31 +0800

    to add most memory query in the function

--- common/remoteproc.cpp
+++ common/remoteproc.cpp
@@ -246,21 +246,21 @@ int ProcEnum(const char * exename,unsigned int ** ppPids,int * pSize)
     }
 
     procinfo.dwSize = sizeof(procinfo);
-	DEBUG_INFO("Start ++++++++++++++++++++\n");
+    DEBUG_INFO("Start ++++++++++++++++++++\n");
     for(i=0,procinfo.dwSize = sizeof(procinfo),bret=Process32First(hSnap,&procinfo); bret; i++,procinfo.dwSize=sizeof(procinfo),bret=Process32Next(hSnap,&procinfo))
     {
 
 #ifdef _UNICODE
         ret = _wcsicmp(pExeNameW,procinfo.szExeFile);
-        DEBUG_INFO("szExeFile %S pExeNameW %S(%s) (%d)\n",procinfo.szExeFile,pExeNameW,exename ,ret);
+        //DEBUG_INFO("szExeFile %S pExeNameW %S(%s) (%d)\n",procinfo.szExeFile,pExeNameW,exename ,ret);
 #else
         ret = _stricmp(exename,procinfo.szExeFile);
-		DEBUG_INFO("szExeFile %s exename(%s) (%d)\n",procinfo.szExeFile,exename,ret);
+        DEBUG_INFO("szExeFile %s exename(%s) (%d)\n",procinfo.szExeFile,exename,ret);
 #endif
         if(ret == 0)
         {
             /*it is compare ok ,so we should set it for the compare */
-			DEBUG_INFO("Find ||||||||||||||||||||||||||||| parent processid %d\n",procinfo.th32ParentProcessID);
+            DEBUG_INFO("Find ||||||||||||||||||||||||||||| parent processid %d\n",procinfo.th32ParentProcessID);
 
             if(retsize <(count + 1))
             {
@@ -286,10 +286,10 @@ int ProcEnum(const char * exename,unsigned int ** ppPids,int * pSize)
                 pRetPids[count] = procinfo.th32ProcessID;
                 pTmpPids = NULL;
             }
-			count ++;
+            count ++;
         }
     }
-	DEBUG_INFO("End ++++++++++++++++++++\n");
+    DEBUG_INFO("End ++++++++++++++++++++\n");
 
     ret = LAST_ERROR_CODE();
     if(ret != ERROR_NO_MORE_FILES)
@@ -329,6 +329,67 @@ fail:
     SetLastError(ret);
     return -ret;
 }
+
+#if 0
+int ProcMemorySize(unsigned int processid,unsigned int*pMemSize)
+{
+    HEAPLIST32 hl;
+    int ret=0;
+    HANDLE hHeapSnap = CreateToolhelp32Snapshot(TH32CS_SNAPHEAPLIST, processid);
+    unsigned int blksize=0;
+
+    hl.dwSize = sizeof(HEAPLIST32);
+
+    if(hHeapSnap == INVALID_HANDLE_VALUE)
+    {
+        ERROR_INFO("CreateToolhelp32Snapshot failed (%d)\n", GetLastError());
+        return -1;
+    }
+
+    *pMemSize = 0;
+    if(Heap32ListFirst(hHeapSnap, &hl))
+    {
+        do
+        {
+            HEAPENTRY32 he;
+            ZeroMemory(&he, sizeof(HEAPENTRY32));
+            he.dwSize = sizeof(HEAPENTRY32);
+
+            if(Heap32First(&he, processid, hl.th32HeapID))
+            {
+                //DEBUG_INFO("\nHeap ID: %d\n", hl.th32HeapID);
+                do
+                {
+                    //DEBUG_INFO("Block size: %d\n", he.dwBlockSize);
+                    blksize += he.dwBlockSize;
+
+                    he.dwSize = sizeof(HEAPENTRY32);
+                }
+                while(Heap32Next(&he));
+            }
+            hl.dwSize = sizeof(HEAPLIST32);
+        }
+        while(Heap32ListNext(hHeapSnap, &hl));
+
+        *pMemSize = blksize;
+        DEBUG_INFO("processid %d memsize 0x%08x\n",processid,blksize);
+    }
+    else
+    {
+        ERROR_INFO("Cannot list first heap (%d)\n", GetLastError());
+        ret = -3;
+    }
+
+    CloseHandle(hHeapSnap);
+
+    return ret;
+}
+
+#else
+
+#if 0
+
+
 
 int ProcMemorySize(unsigned int processid,unsigned int * pMemSize)
 {
@@ -371,6 +432,108 @@ fail:
     hProc = NULL;
     return -ret;
 }
+
+
+int ProcMemorySize(unsigned int processid,unsigned int * pMemSize)
+{
+    int ret;
+    HANDLE hSnap=INVALID_HANDLE_VALUE;
+    HEAPLIST32 heapinfo;
+    int count =0;
+    int i,j;
+    BOOL bret,b2ret;
+    HEAPENTRY32 hentry;
+    unsigned int blksize=0;
+
+    hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPHEAPLIST,processid);
+    if(hSnap == INVALID_HANDLE_VALUE)
+    {
+        ret = LAST_ERROR_CODE();
+        ERROR_INFO("could not get the create snap shot error(%d)\n",ret);
+        goto fail;
+    }
+
+    SetLastError(0);
+    for(i=0,heapinfo.dwSize = sizeof(heapinfo),bret=Heap32ListFirst(hSnap,&heapinfo); bret; i++,heapinfo.dwSize=sizeof(heapinfo),bret=Heap32ListNext(hSnap,&heapinfo))
+    {
+        DEBUG_INFO("heap processid %d\n",heapinfo.th32ProcessID);
+        if(processid == heapinfo.th32ProcessID)
+        {
+            /*it is compare ok ,so we should set it for the compare */
+            SetLastError(0);
+            for(j=0,hentry.dwSize = sizeof(hentry),b2ret=Heap32First(&hentry,processid,heapinfo.th32HeapID); !b2ret; j++,hentry.dwSize = sizeof(hentry),b2ret=Heap32Next(&hentry))
+            {
+                blksize += hentry.dwBlockSize;
+                DEBUG_INFO("Get Processid(%d) heapid (%d) size 0x%08x\n",processid,heapinfo.th32HeapID,hentry.dwBlockSize);
+            }
+
+            ret = LAST_ERROR_CODE();
+            if(ret != ERROR_NO_MORE_FILES)
+            {
+                ERROR_INFO("could not get processid (%d) heapid (%d) error %d\n",processid,heapinfo.th32HeapID,ret);
+                goto fail;
+            }
+            SetLastError(0);
+
+        }
+    }
+
+    ret = LAST_ERROR_CODE();
+
+    if(ret != ERROR_NO_MORE_FILES)
+    {
+        ret = LAST_ERROR_CODE();
+        goto fail;
+    }
+    *pMemSize = blksize;
+
+    CloseHandle(hSnap);
+    return 1;
+fail:
+    assert(ret > 0);
+
+    if(hSnap != INVALID_HANDLE_VALUE)
+    {
+        CloseHandle(hSnap);
+    }
+    hSnap = INVALID_HANDLE_VALUE;
+    SetLastError(ret);
+    return -ret;
+}
+
+#else
+
+
+int ProcMemorySize(unsigned int processid ,unsigned int* pMemSize)
+{
+    HANDLE hProc=NULL;
+    int ret;
+
+    hProc = OpenProcess(PROCESS_QUERY_INFORMATION,FALSE,processid);
+    if(hProc == NULL)
+    {
+    	ret = LAST_ERROR_CODE();
+		ERROR_INFO("could not open process %d error(%d)\n",processid,ret);
+		goto fail;
+    }
+
+	DEBUG_INFO("processid %d open handle 0x%08x\n",processid,hProc);
+
+	CloseHandle(hProc);
+
+	return 0;
+
+fail:
+    if(hProc)
+    {
+        CloseHandle(hProc);
+    }
+    hProc = NULL;
+    return ret;
+}
+
+#endif
+#endif
 
 int EnableDebugLevel(int enable)
 {
