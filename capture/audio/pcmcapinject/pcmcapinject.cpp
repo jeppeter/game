e822703f0e0ef6c67c6cbd50d6a7ccc8d556d5bb
jeppeter <jeppeter@gmail.com>
2013-09-23 17:29:06 +0800

    to make the copy of waveformatex

--- capture/audio/pcmcapinject/pcmcapinject.cpp
+++ capture/audio/pcmcapinject/pcmcapinject.cpp
@@ -3,9 +3,49 @@
 
 #include "stdafx.h"
 #include "pcmcapinject.h"
+#include <Mmreg.h>
+#include <stdlib.h>
 
+#define LAST_ERROR_CODE() (GetLastError() ? GetLastError() : 1)
 
 static CRITICAL_SECTION st_DetourCS;
+static CRITICAL_SECTION st_StateCS;
+
+static WAVEFORMATEX *st_pFormatEx;
+
+static int SetFormat(WAVEFORMATEX* pFormatEx)
+{
+	int ret=0;
+	int formatsize=0;
+	EnterCriticalSection(&st_StateCS);
+
+	if (st_pFormatEx)
+	{
+		free(st_pFormatEx);
+	}
+	st_pFormatEx = NULL;
+
+	/*now to test for the format size*/
+	formatsize = sizeof(*pFormatEx);
+	formatsize += pFormatEx->cbSize;
+
+	st_pFormatEx = (WAVEFORMATEX*)malloc(formatsize);
+	if (st_pFormatEx==NULL)
+	{
+		ret = LAST_ERROR_CODE();
+		goto release;
+	}
+
+	memcpy(st_pFormatEx,pFormatEx,formatsize);
+	
+release:
+	LeaveCriticalSection(&st_StateCS);
+	return ret > 0 ? -ret : 0;
+}
+
+
+
+
 
 /*now we should handle the thread*/
 int PcmCapInject_SetAudioOperation(int iOperation)
