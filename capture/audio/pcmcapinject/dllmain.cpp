c40e3e20fae63ea239ab51a0a7e31fc33e73ade6
jeppeter <jeppeter@gmail.com>
2013-09-23 15:37:00 +0800

    add controldemo into repo

--- capture/audio/pcmcapinject/dllmain.cpp
+++ capture/audio/pcmcapinject/dllmain.cpp
@@ -9,8 +9,10 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
+		break;
 	case DLL_THREAD_ATTACH:
 	case DLL_THREAD_DETACH:
+		break;
 	case DLL_PROCESS_DETACH:
 		break;
 	}
--- capture/audio/pcmcapinject/pcmcapinject.h
+++ capture/audio/pcmcapinject/pcmcapinject.h
@@ -4,6 +4,9 @@
 // 任何其他项目上不应定义此符号。这样，源文件中包含此文件的任何其他项目都会将
 // PCMCAPINJECT_API 函数视为是从 DLL 导入的，而此 DLL 则将用此宏定义的
 // 符号视为是被导出的。
+
+#ifndef __PCMCAP_INJECT_H__
+#define __PCMCAP_INJECT_H__
 #ifdef PCMCAPINJECT_EXPORTS
 #define PCMCAPINJECT_API __declspec(dllexport)
 #else
@@ -19,4 +22,6 @@ public:
 
 extern PCMCAPINJECT_API int npcmcapinject;
 
-PCMCAPINJECT_API int fnpcmcapinject(void);
+PCMCAPINJECT_API int;
+
+#endif /*__PCMCAP_INJECT_H__*/
