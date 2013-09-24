6b7a1c9818d104c8b23e46c3f16571e25ca95195
jeppeter <jeppeter@gmail.com>
2013-09-24 09:02:26 +0800

    to add common lib

---
+++ capture/audio/common/output_debug.h
@@ -0,0 +1,37 @@
+#ifndef  __OUTPUT_DEBUG_H__
+#define  __OUTPUT_DEBUG_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef WIN7_VER
+#define  DEBUG_INFO(fmt,...) DebugOutString(__FILE__,__LINE__,fmt,__VA_ARGS__)
+#define  ERROR_INFO(fmt,...) DebugOutString(__FILE__,__LINE__,fmt,__VA_ARGS__)
+#else
+#ifdef WINXP_VER
+#define  DEBUG_INFO(fmt,...) do{fprintf(stderr,fmt,__VA_ARGS__);DebugOutString(__FILE__,__LINE__,fmt,__VA_ARGS__);}while(0)
+#define  ERROR_INFO(fmt,...) do{fprintf(stderr,fmt,__VA_ARGS__);DebugOutString(__FILE__,__LINE__,fmt,__VA_ARGS__);}while(0)
+#else
+#define  DEBUG_INFO(fmt,...) do{;}while(0)
+#define  ERROR_INFO(fmt,...) do{;}while(0)
+#endif
+#endif
+#define  DEBUG_BUFFER(ptr,blen) DebugBuffer(__FILE__,__LINE__,(unsigned char*)ptr,blen)
+
+
+extern "C" void DebugOutString(const char* file,int lineno,const char* fmt,...);
+extern "C" void DebugBuffer(const char* file,int lineno,unsigned char* pBuffer,int buflen);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
+#endif /*__OUTPUT_DEBUG_H__*/
