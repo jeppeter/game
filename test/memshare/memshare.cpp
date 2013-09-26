aa54d597cf65007bc259bcb0edb3ddf6a4ce631e
jeppeter <jeppeter@gmail.com>
2013-09-26 15:47:02 +0800

    to make test change memshare

--- test/memshare/memshare.cpp
+++ test/memshare/memshare.cpp
@@ -2,10 +2,17 @@
 //
 
 #include "stdafx.h"
+#include "../../common/output_debug.h"
+#include "../../common/uniansi.h"
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	int i;
+	for (i=0;i<argc;i++)
+	{
+		fprintf(stderr,"[%d] %S\n",i,argv[i]);
+	}
 	return 0;
 }
 
