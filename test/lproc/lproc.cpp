dd14d0ac3c7bfdb43ebde8000ce712b3dfdf6b00
jeppeter <jeppeter@gmail.com>
2013-10-14 11:19:45 +0800

    to compile ok for lproc

--- test/lproc/lproc.cpp
+++ test/lproc/lproc.cpp
@@ -2,10 +2,46 @@
 //
 
 #include "stdafx.h"
+#include <procex.h>
+#include <stdlib.h>
+#include <time.h>
+#define ERROR_INFO(...) do{fprintf(stderr,"%s:%s:%d[%ld]\t",__FILE__,__FUNCTION__,__LINE__,time(NULL));fprintf(stderr,__VA_ARGS__);}while(0)
+#define DEBUG_INFO(...) do{fprintf(stderr,"%s:%s:%d[%ld]\t",__FILE__,__FUNCTION__,__LINE__,time(NULL));fprintf(stderr,__VA_ARGS__);}while(0)
 
 
 int main(int argc, char* argv[])
 {
-	return 0;
+	int i,j;
+	unsigned int *pPids=NULL;
+	int pidsize=0,pidnum;
+	int ret;
+	for (i=0;i<argc;i++)
+	{
+		ret= GetModuleInsertedProcess(argv[i],&pPids,&pidsize);
+		if (ret < 0)
+		{
+			ERROR_INFO("get %s pids error(%d)\n",argv[i]);
+		}
+		else
+		{
+			pidnum = ret;
+			DEBUG_INFO("Get %s num(%d)\n",argv[i],pidnum);
+			for (j=0;j<pidnum;j++)
+			{
+				DEBUG_INFO("[%d] %d\n",j,pPids[j]);
+			}
+		}
+		
+	}
+
+	ret = 0;
+out:
+	if (pPids)
+	{
+		free(pPids);
+	}
+	pPids = NULL;
+	pidsize = 0;
+	return -ret;
 }
 
