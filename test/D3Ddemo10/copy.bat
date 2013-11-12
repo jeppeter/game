9a3fb230ac58d048135a3a09d3263fa83e4560e7
jeppeter <jeppeter@gmail.com>
2013-11-12 16:39:42 +0800

    to add D3Ddemo10

---
+++ test/D3Ddemo10/copy.bat
@@ -0,0 +1,10 @@
+set _fromdir=%1
+set _todir=%2
+
+copy %_fromdir%\acce_c1.bmp %_todir%\acce_c1.bmp
+copy %_fromdir%\body_c1.bmp %_todir%\body_c1.bmp
+copy %_fromdir%\face_c1.bmp %_todir%\face_c1.bmp
+copy %_fromdir%\hair_c1.bmp %_todir%\hair_c1.bmp
+copy %_fromdir%\icon.ico %_todir%\icon.ico
+copy %_fromdir%\loli.X %_todir%\loli.X
+copy %_fromdir%\pal5q.jpg %_todir%\pal5q.jpg
