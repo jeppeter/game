2cc0fbcd486066586995030860cdb808843fbf77
jeppeter <jeppeter@gmail.com>
2013-11-22 11:09:41 +0800

    to make a dialog in D3Ddemo10

--- test/tvec/tvec.cpp
+++ test/tvec/tvec.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <stdio.h>
 #include <stdlib.h>
+#include <Windows.h>
 
 typedef struct
 {
@@ -18,7 +19,11 @@ int main(int argc, char* argv[])
     int i;
 
     std::vector<DEMO_t> demovec;
+	std::vector<DEMO_t*>* pdemovec=NULL;
+	DEMO_t* pdemo;
     DEMO_t demo;
+
+	pdemovec = new std::vector<DEMO_t*>();
 
     for(i=0; i<10; i++)
     {
@@ -26,13 +31,21 @@ int main(int argc, char* argv[])
         demo.y = i;
         demo.z = i;
         demovec.push_back(demo);
+		pdemo = new DEMO_t;
+		CopyMemory(pdemo,&demo,sizeof(demo));
+		pdemovec->push_back(pdemo);
     }
 
     while(demovec.size()>0)
     {
+    	pdemo = pdemovec->at(0);
         demo = demovec[0];
         fprintf(stdout,"x %d y %d z %d\n",demo.x,demo.y,demo.z);
+		fprintf(stdout,"px %d py %d pd %d\n",pdemo->x,pdemo->y,pdemo->z);
+		
 		demovec.erase(demovec.begin());
+		pdemovec->erase(pdemovec->begin());
+		delete pdemo;
     }
     return 0;
 }
